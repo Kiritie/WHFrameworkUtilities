f653bf433e0bb4b29acfdd34d224fb80bd0f756a
wuhao <801238014@qq.com>
2024-01-08 13:05:14 +0800

    提交插件内容

---
+++ Source/WHFrameworkUtilities/WHFrameworkUtilities.Build.cs
@@ -0,0 +1,33 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class WHFrameworkUtilities : ModuleRules
+{
+	public WHFrameworkUtilities(ReadOnlyTargetRules Target) : base(Target)
+	{
+		bLegacyPublicIncludePaths = false;
+
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core"
+			});
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"UMG",
+				"Slate",
+				"SlateCore",
+				"CommonUI",
+				"GameplayTags",
+				"WHFramework"
+			});
+	}
+}
